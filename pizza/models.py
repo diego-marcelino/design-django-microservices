6ce40aa3afef59f4906826bb469ae3d4fa5ca74b
Diego Marcelino <diego.arm.mar@gmail.com>
2020-05-28 19:05:21 -0300

    feat: create pizza model representation

--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Pizza(models.Model):
+    """Pizza model representation."""
+
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=240)
